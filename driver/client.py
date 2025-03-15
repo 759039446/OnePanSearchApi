8f6f1dce710affc994c36632a9a6e0a2482a1c92
tua <759039446@qq.com>
2025-03-15 16:50:10 +0800

    pef:重构逻辑，可以通过配置，来请求不同的链接获取资源

--- driver/client.py
+++ driver/client.py
@@ -1,10 +1,11 @@
 import asyncio
 
 from driver.enums import AsyncEnum
+from driver.model.search_result import SearchResult
 from utils.collection_util import sort_results_by_key
 
 
-async def search(keyword: str):
+async def search(keyword: str) -> list[SearchResult]:
     results = await asyncio.gather(*(AsyncEnum.async_handler(x, keyword=keyword, page='1') for x in AsyncEnum))
     results = sort_results_by_key(results, "from_site", ["KK大厅", "kk短剧", "kk橘子资源", "kk小宇"])
     merged_results = [item for sublist in results for item in sublist]
